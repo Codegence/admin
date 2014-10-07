bbf35dcd8baef8f92b0efb2881a3fd059689d1d7
Gaston Kaltner <gaston.kaltner@globallogic.com>
2014-10-07 10:37:47 -0300

    se agrega barra de estado y se mueve la carpeta views a public

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ function clientErrorHandler(err, req, res, next) {
 var app = express();
 app.engine('html',consolidate.swig);
 app.set('view engine','html');
-app.set('views',path.join(__dirname ,"views"));
+app.set('views',path.join(__dirname ,"public/views"));
 app.use(logger('combined'));
 app.use(bodyParser.text());
 app.use(methodOverride());
